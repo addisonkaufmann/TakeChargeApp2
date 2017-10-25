83d37f5950271b63adce2b7283cbd257091ea89c
addisonkaufmann <addisonkaufmann@email.arizona.edu>
2017-10-25 09:26:58 -0700

    calendar styling

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DayPage } from '../day/day';
+
+import { CalendarEvent, CalendarDateFormatter } from 'angular-calendar';
+import { CustomDateFormatter } from '../../providers/custom-date-formatter.provider';
 
 
 
 @Component({
   selector: 'page-calendar',
-  templateUrl: 'calendar.html'
+  templateUrl: 'calendar.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+  	{
+  		provide: CalendarDateFormatter,
+  		useClass: CustomDateFormatter
+  	}
+  ]
 })
 export class CalendarPage {
 
@@ -21,3 +31,27 @@ export class CalendarPage {
 	constructor(public navCtrl: NavController) {}
 	
 }
+
+
+// import { Component, ChangeDetectionStrategy } from '@angular/core';
+// import { CalendarEvent, CalendarDateFormatter } from 'angular-calendar';
+// import { CustomDateFormatter } from './custom-date-formatter.provider';
+
+// @Component({
+//   selector: 'mwl-demo-component',
+//   changeDetection: ChangeDetectionStrategy.OnPush,
+//   templateUrl: 'template.html',
+//   providers: [
+//     {
+//       provide: CalendarDateFormatter,
+//       useClass: CustomDateFormatter
+//     }
+//   ]
+// })
+// export class DemoComponent {
+//   view: string = 'month';
+
+//   viewDate: Date = new Date();
+
+//   events: CalendarEvent[] = [];
+// }
